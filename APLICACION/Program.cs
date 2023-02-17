74c3e62cf26eee912f01020f534694235e85c7e1
Gloribel010 <gloribelc07@gmail.com>
2023-02-16 20:32:43 -0600

    Vista

---
+++ APLICACION/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using APLICACION.Entidades;
+using APLICACION.Negocio;
+
+ClsAlmacenamiento clsalmacenamiento = new ClsAlmacenamiento ( );
+
+Almacenamiento almacenamiento = new Almacenamiento ();
+Console.WriteLine("Ingrese una palabra: ");
+almacenamiento.letras = Console.ReadLine();
+Console.WriteLine( " La cantidad de letras es: "+ almacenamiento.letras?.Length);
