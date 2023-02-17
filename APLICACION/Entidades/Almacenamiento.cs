51f4d880a2665674cee7a9262778e04fbc7253f4
Gloribel010 <gloribelc07@gmail.com>
2023-02-16 20:31:14 -0600

    Entidad variable

---
+++ APLICACION/Entidades/Almacenamiento.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APLICACION.Entidades
+{
+    public class Almacenamiento
+    {
+        public string? letras;
+    }
+}
